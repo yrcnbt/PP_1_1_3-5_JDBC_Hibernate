59a6b7421930b31662210b2badc2a78dd2999432
yrcnbt <yuri.danilenko97@gmail.com>
2023-05-14 12:24:17 +0300

    обновление

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,8 +1,15 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
+
+import static jm.task.core.jdbc.util.Util.getConnection;
 
 public class UserDaoJDBCImpl implements UserDao {
     public UserDaoJDBCImpl() {
@@ -10,19 +17,43 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void createUsersTable() {
-
+        String sql = "CREATE TABLE user(Id INT AUTO_INCREMENT PRIMARY KEY, Name VARCHAR(40), LastName VARCHAR(40), age SMALLINT)";
+        try(Statement statement = Util.getConnection().createStatement()){
+            statement.executeUpdate(sql);
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
     }
 
-    public void dropUsersTable() {
 
+    public void dropUsersTable() {
+        String sql = "DROP TABLE user";
+        try(Statement statement = Util.getConnection().createStatement()){
+            statement.executeUpdate(sql);
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
+        String sql = "INSERT INTO user(NAME, LASTNAME, AGE) VALUES (?, ?, ?)";
+        try(PreparedStatement preparedStatement = Util.getConnection().prepareStatement(sql)){
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, lastName);
+            preparedStatement.setByte(3, age);
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
     }
 
     public void removeUserById(long id) {
+        String sql = "DELETE FROM user WHERE Id IS ?";
 
+        try(PreparedStatement preparedStatement = Util.getConnection().prepareStatement(sql)){
+            preparedStatement.setLong(1, id);
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
     }
 
     public List<User> getAllUsers() {
--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,12 +1,19 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
-public class UserServiceImpl implements UserService {
-    public void createUsersTable() {
+public class UserServiceImpl extends Util implements UserService {
 
+    UserDaoJDBCImpl udji = new UserDaoJDBCImpl();
+    public void createUsersTable() {
     }
 
     public void dropUsersTable() {
